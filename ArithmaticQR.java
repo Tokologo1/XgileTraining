9589c7fa2a066e3304da72a89b4b8cb0c2c6813c
tchabalala <tokologo.chabalala@cbrta.co.za>
2022-04-07 23:05:04 +0200

    updated_version

--- ArithmaticQR.java
+++ ArithmaticQR.java
@@ -10,16 +10,13 @@ public class ArithmaticQR {
         int first_num = sc.nextInt();
         System.out.println("Please Enter your second number: ");
         int Second_num = sc.nextInt();
-        int sum = first_num/Second_num;
-        System.out.println("Total : " +first_num + " / " + Second_num + " = " +sum);
+        int quicient = first_num/Second_num;
+        System.out.println("Total : " +first_num + " / " + Second_num + " = " +quicient);
+        int remainder = first_num % Second_num;
+        System.out.println("Total : " +first_num + " % " + Second_num + " = " +remainder);
 
-        System.out.println("Enter vlues to get a Remainder");
-        System.out.println("Please Enter your first number: ");
-        int first_num1 = sc.nextInt();
-        System.out.println("Please Enter your second number: ");
-        int Second_num1 = sc.nextInt();
-        int sum1 = first_num % Second_num;
-        System.out.println("Total : " +first_num1 + " % " + Second_num1 + " = " +sum1);
+        
+        
 
         
     }
