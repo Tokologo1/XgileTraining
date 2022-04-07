773f84ab0c01c6ecdddbdf785d6e600d0fb36517
tchabalala <tokologo.chabalala@cbrta.co.za>
2022-04-07 22:44:44 +0200

    Additional

---
+++ ArithmaticAdd.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class ArithmaticAdd {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please Enter your first number: ");
+        int first_num = sc.nextInt();
+        System.out.println("Please Enter your second number: ");
+        int Second_num = sc.nextInt();
+
+        int sum = first_num + Second_num;
+        System.out.println("Total : " +first_num + " + " + Second_num + " = " +sum);
+
+      
+    }
+}
