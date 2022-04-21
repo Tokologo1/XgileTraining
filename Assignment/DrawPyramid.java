196bf6242fa58ea8f400f5b76ffd8ef17f1327b0
tchabalala <tokologo.chabalala@cbrta.co.za>
2022-04-21 10:41:14 +0200

    updated

--- Assignment/DrawPyramid.java
+++ Assignment/DrawPyramid.java
@@ -4,25 +4,14 @@ public class DrawPyramid {
 
     public static void main(String[]args)
     {
-        char last = 'E', alphabet = 'A';
-        int rows = 5;
+      int rows = 5;
 
-
-        for (int i = rows; i >= 1; --i) {
-            for (int j = 1; j <= i; ++j) {
-              System.out.print("* ");
-
-              for (i = 1; i <= (last - 'A' + 1); ++i) {
-                System.out.print(alphabet++);
-              }
-
-              System.out.print(alphabet + " ");
-            }
-            System.out.println();
-          }
-
-    
-    }
+      for (int i = rows; i >= 1; --i) {
+        for (int j = 1; j <= i; ++j) {
+          System.out.print("* ");
+        }
+        System.out.println();
+      }
     }
 
     
