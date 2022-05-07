d799058f99f0d021ec3e0535b3979dd1ef184f8d
Tokologo1 <tokocha81@gmail.com>
2022-05-07 09:49:02 +0200

    Update Main.java

--- Assignament2/Employee/Main.java
+++ Assignament2/Employee/Main.java
@@ -3,7 +3,7 @@ import java.util.ArrayList;
 public class Main {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
         
         ArrayList<Object> depts = new ArrayList<Object>();
         Dept deptObject = new Dept();
