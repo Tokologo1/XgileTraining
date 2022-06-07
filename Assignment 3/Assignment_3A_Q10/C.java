1f72ab0343cfb07305ab22eca9901b7d790e13ec
tchabalala <tokologo.chabalala@cbrta.co.za>
2022-06-07 15:02:36 +0200

    Concluded

---
+++ Assignment 3/Assignment_3A_Q10/C.java
@@ -0,0 +1,34 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class C{
+
+    public static void main(String[]args)
+    {
+
+        try{
+            Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        String s = Integer.toString(n);
+        //Read a line and store it in s
+        int a = Integer.parseInt(s);
+        System.out.println("Enter another number: ");
+        //Read a line and store it in s
+        int b = sc.nextInt();
+        b = Integer.parseInt("Product " + a*b);
+
+        }catch(NumberFormatException e){
+            System.out.println("The Number format is not correct"); 
+        }
+        catch(InputMismatchException e){
+            System.out.println("The input is not correct");
+        }
+        
+        finally{
+            System.out.println("The Exception in Handled");
+        }
+        
+        
+    }
+}
